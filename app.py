9868baeed4008dd20e0352c1a2e0c4908a0c9871
typebot-pc <typebot.pc@gmail.com>
2026-01-20 09:36:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-from fastapi import FastAPI
-import os
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {
-        "status": "ok",
-        "mensagem": "Projeto Python rodando no EasyPanel 🚀",
-        "ambiente": os.getenv("ENV", "local")
-    }
+@app.get("/", include_in_schema=False)
+def root():
+    return Response(content="OK", media_type="text/plain")
 
 @app.get("/health")
 def health():
-    return {"health": "ok"}
+    return {"status": "ok"}
